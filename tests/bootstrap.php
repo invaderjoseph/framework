5b0a64ba7f7011adea8dcee05766053876911a5c
thavarshan <tjthavarshan@gmail.com>
2020-06-10 07:45:09 +0530

    Prepare for new development branch

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Register The Composer Auto Loader
+ */
+require dirname(__DIR__ . '/vendor/autoload.php');
+
+/*
+ * Set The Default Timezone
+ */
+date_default_timezone_set('UTC');
+
+setlocale(LC_ALL, 'C.UTF-8');
