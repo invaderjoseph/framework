9da2c33292da939ffb698c980c812bb622e3038e
thavarshan <tjthavarshan@gmail.com>
2020-07-13 01:37:24 +0530

    Add comments to all methods of container usage process

--- src/Emberfuse/Container/Container.php
+++ src/Emberfuse/Container/Container.php
@@ -47,6 +47,7 @@ final class Container implements ContainerInterface
      */
     public function has($id)
     {
+        // Determine if the given binding has already been registered to the container.
         return isset($this->bindings[$id]) || isset($this->instances[$id]);
     }
 
@@ -59,6 +60,7 @@ final class Container implements ContainerInterface
      */
     private function isShared(string $abstract): bool
     {
+        // Determine if the given binding is set to be a sharable/singleton instance.
         return isset($this->instances[$abstract]) ||
             (isset($this->bindings[$abstract]['shared']) &&
             true === $this->bindings[$abstract]['shared']);
@@ -74,6 +76,7 @@ final class Container implements ContainerInterface
      */
     public function singleton(string $abstract, $concrete = null): void
     {
+        // Bind a sharable instance of a binding to the service container.
         $this->bind($abstract, $concrete, true);
     }
 
@@ -88,18 +91,31 @@ final class Container implements ContainerInterface
      */
     public function bind(string $abstract, $concrete = null, bool $shared = false): void
     {
+        // Determine if a binsing with same abstract type exists.
         if ($this->has($abstract)) {
+            // If so, remove it from the register and make a fresh registration.
             $this->dropStaleInstances($abstract);
         }
 
+        // Determine if the concrete type is provided.
         if (is_null($concrete)) {
+            // If it is not, assign the abstract type to the concrete.
+            // This is usually because the abstract is a name of a class
+            // and the class is expected to be automatically resolved
+            // during the binding process.
             $concrete = $abstract;
         }
 
+        // Determine if the provided concrete type is a callable
         if (!$concrete instanceof Closure) {
+            // If not, it is probably because the concrete is set to be the same
+            // as the abstract type and so it needs to be resolved and wrapped inside
+            // a callable function.
             $concrete = $this->makeClosure($concrete);
         }
 
+        // Bind the abstract and concrete types into the container registry as
+        // one of the container's bindings.
         $this->bindings[$abstract] = compact('concrete', 'shared');
     }
 
@@ -112,6 +128,8 @@ final class Container implements ContainerInterface
      */
     protected function makeClosure(string $concrete): Closure
     {
+        // Use the container as an argument so the given concrete type which is
+        // usually a class name can be resolved during the "make/resolve" process.
         return function ($container) use ($concrete) {
             return $container->build($concrete);
         };
@@ -123,8 +141,12 @@ final class Container implements ContainerInterface
     public function get($id)
     {
         try {
+            // Try to resolve the given binding from the container.
             return $this->make($id);
         } catch (BindingResolution $exception) {
+            // If an exception was thrown it is either because the binding does
+            // not exist within the container or an error occurred during the resolution
+            // process of the given binding.
             if ($this->has($id)) {
                 throw $exception;
             }
@@ -145,6 +167,8 @@ final class Container implements ContainerInterface
      */
     public function make(string $abstract, array $parameters = [])
     {
+        // Use the "resolve" method to resolve the given binding from the container
+        // or instantly resolve the class type for usage,
         return $this->resolve($abstract, $parameters);
     }
 
@@ -160,18 +184,25 @@ final class Container implements ContainerInterface
      */
     private function resolve(string $abstract, array $parameters = [])
     {
+        // Get concrete type from the container registry.
         $concrete = $this->getConcrete($abstract);
 
+        // Determine if a sharable instance already exists within the container.
         if (isset($this->instances[$abstract]) && $parameters === []) {
+            // If so, return the sharable instance.
             return $this->instances[$abstract];
         }
 
+        // Otherwise the type required to be instantiated or called upon.
         $object = $this->build($concrete, $parameters);
 
+        // Determine if the binding is set to be a singleton/sharable instance.
         if ($this->isShared($abstract)) {
+            // If so, save the resolved instance in the sharable instances registry.
             $this->instances[$abstract] = $object;
         }
 
+        // Return resolved results.
         return $object;
     }
 
@@ -184,10 +215,14 @@ final class Container implements ContainerInterface
      */
     private function getConcrete(string $abstract)
     {
+        // Determine if a concrete type is bound in the container.
         if (isset($this->bindings[$abstract])) {
             return $this->bindings[$abstract]['concrete'];
         }
 
+        // If no concrete type was found, the given abstract type was not bound
+        // inside the container, so return the given string as the concrete type to be
+        // resolved as a class.
         return $abstract;
     }
 
@@ -203,37 +238,58 @@ final class Container implements ContainerInterface
      */
     private function build($concrete, array $parameters = [])
     {
+        // Determine if the given concrete type is callable.
         if ($concrete instanceof Closure) {
+            // If so, execute the callable functions with appropriate parameters.
             return call_user_func_array($concrete, array_merge([$this], $parameters));
         }
 
+        // If the given concrete type is not a callable, it means it is a string
+        // that is a class name and has to be instantiated.
         try {
+            // Make new reflection class object of the concrete type.
+            // This is done so that the parameters/dependencies of the given class can
+            // also be resolved recursively.
             $reflector = new ReflectionClass($concrete);
         } catch (ReflectionException $exception) {
+            // If an exception was caught it means the concrete type is not a class name
+            // and cannot be resolved.
             $this->throwBindingResolutionException($concrete, $exception);
         }
 
+        // Determine if the class is not instantiable.
         if (!$reflector->isInstantiable()) {
+            // If so, throw a resolution exception.
             return $this->throwBindingResolutionException(
                 "Target [$concrete] is not instantiable."
             );
         }
 
+        // Get the constructor method of the class.
         $constructor = $reflector->getConstructor();
 
+        // Determine if the class has a constructor method.
         if (is_null($constructor)) {
+            // If not, it means the class has no dependencies and can be
+            // instantiated immediately.
             return new $concrete();
         }
 
+        // Get all parameters from the class constructor.
         $dependencies = $constructor->getParameters();
 
         try {
-            $instances = $this->resolveDependencies($dependencies);
+            // Resolve all dependencies to it's relevant types.
+            $dependencies = $this->resolveDependencies(
+                array_merge($dependencies, $parameters)
+            );
         } catch (BindingResolution $exception) {
             throw $exception;
         }
 
-        return $reflector->newInstanceArgs($instances);
+        // return a new instance of the now resolving class with all appropriate
+        // dependencies (resolved dependencies).
+        return $reflector->newInstanceArgs($dependencies);
     }
 
     /**
@@ -249,12 +305,17 @@ final class Container implements ContainerInterface
     {
         $results = [];
 
+        // Iterate through each set of parameters.
         foreach ($dependencies as $dependency) {
+            // Determine and resolve each type of the parameters.
             $results[] = is_null($dependency->getClass())
+                // Primitive types include, "string", "array", "integer" and "boolean".
                 ? $this->resolvePrimitive($dependency)
+                // These are parameters with class names.
                 : $this->resolveClass($dependency);
         }
 
+        // Return resolved results.
         return $results;
     }
 
@@ -269,10 +330,13 @@ final class Container implements ContainerInterface
      */
     protected function resolvePrimitive(ReflectionParameter $parameter)
     {
+        // Determine if the parameters have default values assigned to them.
         if ($parameter->isDefaultValueAvailable()) {
+            // If so, return the default values.
             return $parameter->getDefaultValue();
         }
 
+        // Else throw primitive is unresolvable error.
         $this->throwBindingResolutionException(
             "Unresolvable dependency resolving [$parameter] in class {$parameter->getDeclaringClass()->getName()}"
         );
@@ -289,6 +353,7 @@ final class Container implements ContainerInterface
      */
     protected function resolveClass(ReflectionParameter $parameter)
     {
+        // Recursively resolve the given object type.
         return $this->make($parameter->getClass()->name);
     }
 
@@ -314,10 +379,14 @@ final class Container implements ContainerInterface
      */
     public static function getInstance()
     {
+        // Determine if a globally available instance of the
+        // service container is available to access.
         if (is_null(static::$instance)) {
+            // If not, make new instance and set self as instance.
             static::makeInstance(new static());
         }
 
+        // Return globally available instance of the container.
         return static::$instance;
     }
 
@@ -330,6 +399,7 @@ final class Container implements ContainerInterface
      */
     public static function makeInstance(?ContainerInterface $container = null)
     {
+        // Set default sharable insensate of the service container.
         return static::$instance = $container;
     }
 
@@ -342,6 +412,7 @@ final class Container implements ContainerInterface
      */
     private function dropStaleInstances(string $abstract): void
     {
+        // Remove already existing instances of given binding.
         unset($this->instances[$abstract], $this->aliases[$abstract]);
     }
 }
