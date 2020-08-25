180719226adeeab3772ae61b0f2a637239018622
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-08-26 01:20:59 +0530

    Add router module with add route method

--- tests/Routing/RouteCollectionTest.php
+++ tests/Routing/RouteCollectionTest.php
@@ -5,10 +5,16 @@ namespace Emberfuse\Tests\Routing;
 use Emberfuse\Routing\Route;
 use PHPUnit\Framework\TestCase;
 use Emberfuse\Routing\RouteCollection;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class RouteCollectionTest extends TestCase
 {
+    public function testItAddsGivenRouteToCollection()
+    {
+        $routeCollection = new RouteCollection();
 
+        $routeCollection->add(new Route('GET', 'foo', 'FooController@bar'));
+
+        $this->assertCount(1, $routeCollection);
+        $this->assertCount(1, $routeCollection->getRoutes());
+    }
 }
