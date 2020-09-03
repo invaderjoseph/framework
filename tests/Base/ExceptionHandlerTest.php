f5ac3268f0bb956f000ccb06af8d9b3271a3924f
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-09-03 14:23:18 +0530

    Add exception handler module tests

--- tests/Base/ExceptionHandlerTest.php
+++ tests/Base/ExceptionHandlerTest.php
@@ -2,12 +2,52 @@
 
 namespace Emberfuse\Tests\Base;
 
+use Mockery;
+use RuntimeException;
+use Psr\Log\LoggerInterface;
 use Emberfuse\Tests\TestCase;
+use Emberfuse\Base\ExceptionHandler;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ExceptionHandlerTest extends TestCase
 {
-    public function testBaseicExceptionHandler()
+    public function tearDown(): void
     {
-        $this->assertTrue(true);
+        Mockery::close();
+    }
+
+    public function testExceptionReporting()
+    {
+        $logger = Mockery::mock(LoggerInterface::class);
+        $logger->shouldReceive('error')->withArgs(['Exception message', Mockery::hasKey('exception')]);
+        $handler = new ExceptionHandler($logger);
+        $handler->report(new RuntimeException('Exception message'));
+    }
+
+    public function testExceptionRendering()
+    {
+        putenv('APP_DEBUG=true');
+        $request = Mockery::mock(Request::class);
+        $logger = Mockery::mock(LoggerInterface::class);
+        $handler = new ExceptionHandler($logger);
+
+        $response = $handler->render($request, new RuntimeException('Exception message'));
+
+        $this->assertStringContainsString('Exception message', $response->getContent());
+        $this->assertEquals(500, $response->getStatusCode());
+    }
+
+    public function testHttpExceptionRendering()
+    {
+        putenv('APP_DEBUG=true');
+        $request = Mockery::mock(Request::class);
+        $logger = Mockery::mock(LoggerInterface::class);
+        $handler = new ExceptionHandler($logger);
+
+        $response = $handler->render($request, new HttpException(403, 'Http exception message'));
+
+        $this->assertStringContainsString('Http exception message', $response->getContent());
+        $this->assertEquals(403, $response->getStatusCode());
     }
 }
